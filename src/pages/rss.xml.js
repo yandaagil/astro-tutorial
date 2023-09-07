f9455de90bd9bbdbe93c7dec985783ef0a696d2c
Yanda Agil <yandaagil@gmail.com>
2023-09-07 19:55:06 +0700

    refactor: code cleaning

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export async function GET() {
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
-    site: 'https://astro-tutorial-yanda.vercel.app/',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    title: "Astro Learner | Blog",
+    description: "My journey learning Astro",
+    site: "https://astro-tutorial-yanda.vercel.app/",
+    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
     customData: `<language>en-us</language>`,
   });
 }
